a32e6f896f1b872b8817d87d0fd247912a663777
Shu Uesugi <shu@chibicode.com>
2014-04-19 10:50:24 -0700

    write

--- _assets/javascripts/main.coffee
+++ _assets/javascripts/main.coffee
@@ -1,3 +1,6 @@
 $ ->
   $("a[href^='http']").attr("target","_blank")
   $(".post").fitVids()
+
+$(window).load ->
+  $(".js-toplink").show().addClass("magictime foolishIn")
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,3 +2,7 @@ $(function() {
   $("a[href^='http']").attr("target", "_blank");
   return $(".post").fitVids();
 });
+
+$(window).load(function() {
+  return $(".js-toplink").show().addClass("magictime foolishIn");
+});
