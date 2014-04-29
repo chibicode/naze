066acd13c57589c27e653531ce08d80856d61d35
Shu Uesugi <shu@chibicode.com>
2014-04-28 20:56:45 -0700

    write

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,7 +2,3 @@ $(function() {
   $("a[href^='http']").attr("target", "_blank");
   return $(".post").fitVids();
 });
-
-$(window).load(function() {
-  return $(".js-toplink").show().addClass("magictime foolishIn");
-});
